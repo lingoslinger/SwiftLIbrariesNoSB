4aab65cf1dc6b71b085cb182e48d98bb841eac77
Allan Evans <lingoslinger@me.com>
2021-09-21 12:43:08 -0500

    Initial Commit

---
+++ SwiftLibrariesNoSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLibrariesNoSB
+//
+//  Created by Allan Evans on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
