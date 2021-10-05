d25b9008835247b5d4dd93561b1961d840194b84
Allan Evans <lingoslinger@me.com>
2021-10-05 09:39:18 -0500

    seperated UI elements from detail view

---
+++ SwiftLibrariesNoSB/UI Elements/PhoneUITextView.swift
@@ -0,0 +1,20 @@
+//
+//  PhoneUITextView.swift
+//  SwiftLibrariesNoSB
+//
+//  Created by Allan Evans on 10/5/21.
+//
+
+import UIKit
+
+class PhoneUITextView: UITextView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
